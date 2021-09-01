88818519295bca0c0afa6d309123b7ddb07e4920
Andrei Toader <andreitoader@Florinas-MacBook-Air.local>
2021-09-01 22:17:44 +0300

    basic node

--- concepts/client.py
+++ concepts/client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3.8
 
 # WSS (WS over TLS) client example, with a self-signed certificate
 
--- flash_pymas/core/node.py
+++ flash_pymas/core/node.py
@@ -4,66 +4,38 @@ import logging
 
 class Node(EntityInterface):
     def __init__(self, name=None):
-        self._isRunning = False
+        self._running = False
         self._name = name
         self._registered_entities = dict()
-         
+        self._entity_order = []
     
     def register_entity(self, entity_name:str, entity:EntityInterface, entity_type:str):
-        pass
+        self._entity_order.append(entity)
+		if entity_type not in self._registered_entities:
+			self._registered_entities[entity_type] = []
+			self._registered_entities[entity_type].append(entity)
+
+	def __enter__(self):
+		self.start()
+		return self
+      
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+       self.stop()
 
     def start(self):
-        logging.info(f"Starting node {self._name}...") 
-        for entity in self._registered_entities:
-            logging.info(f"Starting entity {entity}...")
-            self._registered_entities[entity].start()
-        self._isRunning=True
-        logging.info(f"Node {self._name} started.") 
+		if not self.isRunning:
+			logging.info(f"Starting node {self._name}...") 
+			for entity in self._entity_order:
+				logging.info(f"Starting entity {entity.getName}...")
+				entity.start()
+			self._running=True
+			logging.info(f"Node {self._name} started.") 
 
-        
-    
-
-    # public boolean start()
-	# {
-	# 	li("Starting node [].", name);
-	# 	for(Entity<?> entity : entityOrder) {
-	# 		String entityName = entity.getName();
-	# 		lf("starting entity []...", entityName);
-	# 		if(entity.start()) {
-	# 			lf("entity [] started successfully.", entityName);
-	# 			if(getName() != null && (entity instanceof DefaultPylonImplementation))
-	# 				messagingShard.register(getName());
-	# 		}
-	# 		else
-	# 			le("failed to start entity [].", entityName);
-	# 	}
-	# 	isRunning = true;
-	# 	sendStatusUpdate();
-	# 	li("Node [] started.", name);
-		
-	# 	if(getName() != null && registerEntitiesToCentralEntity())
-	# 		lf("Entities successfully registered to control entity.");
-	# 	return true;
-	# }
-	
-	# @Override
-	# public boolean stop()
-	# {
-	# 	li("Stopping node [].", name);
-	# 	LinkedList<Entity<?>> reversed = new LinkedList<>(entityOrder);
-	# 	Collections.reverse(reversed);
-	# 	for(Entity<?> entity : reversed) {
-	# 		if(entity.isRunning())
-	# 		{
-	# 			lf("stopping an entity...");
-	# 			if(entity.stop())
-	# 				lf("entity stopped successfully.");
-	# 			else
-	# 				le("failed to stop entity.");
-	# 		}
-	# 	}
-	# 	isRunning = false;
-	# 	sendStatusUpdate();
-	# 	li("Node [] stopped.", name);
-	# 	return true;
-	# }
+	def stop(self):
+		if self.isRunning:
+			logging.info(f"Stopping node {self._name}...")
+			for entity in self._entity_order[::-1]:
+				logging.info(f"Stopping entity {entity.getName}...")
+				entity.stop()
+			self._running=False
+			logging.info(f"Node {self._name} stopped.") 
